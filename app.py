6bea9e83cfd4371d9baef3327b6f1fd0e9c9fbe2
MEENAKSHI SHARMA <110449407+Dronzer001@users.noreply.github.com>
2026-04-21 09:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,24 @@
 # if st.button("Predict"):
 #     result = model.predict([[x]])
 #     st.success(result[0])
-print("APP STARTING...")
+# print("APP STARTING...")
 
+# import streamlit as st
+
+# st.title("Azure test app working 🚀")
+# st.write("If you see this, Streamlit is running correctly")
 import streamlit as st
+import pickle
+import os
 
-st.title("Azure test app working 🚀")
-st.write("If you see this, Streamlit is running correctly")
+st.title("Simple ML App")
+
+# safe model loading
+model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
+model = pickle.load(open(model_path, "rb"))
+
+x = st.number_input("Enter value")
+
+if st.button("Predict"):
+    result = model.predict([[x]])
+    st.success(result[0])
